1dfb812490e6a4d2abda3a30f8f8119c8f538c61
Pneuma <syca0714@gmail.com>
2020-04-22 08:14:03 +0900

    Update index (test).js

--- index (test).js
+++ index (test).js
@@ -4,7 +4,7 @@ const canvas = document.getElementById('game'),
 
 let nickname = undefined, response = new Array;
 
-const script = await fetch("test.json").json();
+const script = fetch("test.json").json();
 
 confirm = (title, id, placeholder, func) => {
     Swal.fire({
