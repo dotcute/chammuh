4592c6fbb818b527748293e2088d55ee59ef9717
Pneuma <syca0714@gmail.com>
2020-04-23 13:40:36 +0900

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById('game'),
   ctx = canvas.getContext('2d'),
-  logo = new Image(),
+  image = new Image(),
   httpRequest = new XMLHttpRequest();
 
 let nickname = undefined, response = new Array;
@@ -41,17 +41,18 @@ Swal.fire({
 let isClick = false;
 
 const playScript = (json) => {
+  if (!json) alert('존재하지 않는 스크립트입니다.');
   playScene(json, 'main');
-}
+};
 
 const playScene = (script, name) => {
   return new Promise(async (resolve, reject) => {
     const scene = script[name];
-    if (!scene) alert(`장면 '${name}' 이 존재하지 않습니다.`)
+
+    if (!scene) alert(`장면 '${name}' 이 존재하지 않습니다.`);
 
     for (let s of scene) {
-
-      if (s.trigger && !eval(s.trigger)) continue
+      if (s.trigger && !eval(s.trigger)) continue;
 
       switch (s.type) {
         case 'conv':
@@ -67,47 +68,41 @@ const playScene = (script, name) => {
           alert('정의되지 않은 type 값입니다.');
           break;
       }
-      if (s.script) eval(s.script)
+      if (s.script) eval(s.script);
     }
-    resolve()
-  })
+    resolve();
+  });
 };
 
 const playConv = (content) => {
   return new Promise(async (resolve, reject) => {
-    for (let c of content) {
-      console.log(c)
-      await waitUntilClick(c);
-    }
-    resolve()
-  })
-}
+    for (let c of content) await waitUntilClick(c);
+    resolve();
+  });
+};
 
 const playQues = (content, answer) => {
   return new Promise(async (resolve, reject) => {
-    for (let c of content) {
-      console.log(c)
-      await waitUntilClick(c);
-    }
-    resolve()
-  })
-}
+    for (let c of content) await waitUntilClick(c);
+    resolve();
+  });
+};
 
 const waitUntilClick = (content) => {
   return new Promise(async (resolve, reject) => {
     let loop = setInterval(() => {
       if (isClick) {
         printText(eval(`\`${content[0]}\``), content[1])
-        isClick = false
-        resolve()
-        clearInterval(loop)
+        isClick = false;
+        resolve();
+        clearInterval(loop);
       }
-    }, 100)
-  })
-}
+    }, 100);
+  });
+};
 
 const printText = (text, img) => {
-  if (img) logo.src = `https://rawcdn.githack.com/EntryJSers/chammuh_assets/master/assets/${img}.png`
+  if (img) image.src = `https://rawcdn.githack.com/EntryJSers/chammuh_assets/master/assets/${img}.png`
   ctx.clearRect(0, 430, canvas.width, canvas.height)
   ctx.font = '24px Spoqa Han Sans'
   ctx.fillStyle = 'white'
@@ -118,7 +113,8 @@ const printText = (text, img) => {
 
 let count = 0
 
-fetch('https://raw.githack.com/EntryJSers/chammuh/master/scripts/script.json')
+// https://raw.githack.com/EntryJSers/chammuh/master/scripts/script.json
+fetch('./scripts/script.json')
   .then(res => res.json())
   .then(json => playScript(json))
 
@@ -126,8 +122,8 @@ window.addEventListener('load', () => {
   printText('클릭하여 시작...', 'title')
 });
 
-logo.addEventListener('load', () => {
-  ctx.drawImage(logo, 0, 0, 960, 420)
+image.addEventListener('load', () => {
+  ctx.drawImage(image, 0, 0, 960, 420)
 }, false);
 
 canvas.addEventListener('click', () => {
