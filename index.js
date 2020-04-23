3d049c96a2fd5d5744759bc20f1401e5afc10586
Pneuma <syca0714@gmail.com>
2020-04-23 10:25:37 +0900

    .

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ const printText = (text, img) => {
 
 let count = 0
 
-fetch('https://raw.githack.com/EntryJSers/chammuh/master/script.json')
+fetch('https://raw.githack.com/EntryJSers/chammuh/master/test.json')
   .then(res => res.json())
   .then(json => playScript(json))
 
