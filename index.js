411970464ab3719e312c1f1cbaef131c8f887a83
Pneuma <syca0714@gmail.com>
2020-04-24 20:57:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ confirm('이름이 뭐야?', 'nameInput', '홍길동', (id) => {
 
 // ====================================================================================================
 
-CanvasRenderingContext2D.prototype.roundRect = (x, y, w, h, r) => {
+CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
   if (w < 2 * r) r = w / 2;
   if (h < 2 * r) r = h / 2;
   this.beginPath();
