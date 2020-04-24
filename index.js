8ce1d870651be90fc5943d6a9cea73df04cde0fc
Pneuma <syca0714@gmail.com>
2020-04-24 20:46:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,21 @@ confirm('이름이 뭐야?', 'nameInput', '홍길동', (id) => {
 
 // ====================================================================================================
 
+CanvasRenderingContext2D.prototype.roundRect = (x, y, w, h, r) => {
+  if (w < 2 * r) r = w / 2;
+  if (h < 2 * r) r = h / 2;
+  this.beginPath();
+  this.moveTo(x+r, y);
+  this.arcTo(x+w, y,   x+w, y+h, r);
+  this.arcTo(x+w, y+h, x,   y+h, r);
+  this.arcTo(x,   y+h, x,   y,   r);
+  this.arcTo(x,   y,   x+w, y,   r);
+  this.closePath();
+  return this;
+}
+
+// ====================================================================================================
+
 let isClick = false;
 
 const playScript = (json) => {
@@ -141,7 +156,8 @@ const show = (text, img, smooth=true) => {
 
 const showTalker = (name) => {
   ctx.lineWidth = 5;
-  ctx.strokeRect(40, 440, ctx.measureText(name).width + 15, 40);
+  // ctx.strokeRect(40, 440, ctx.measureText(name).width + 15, 40);
+  ctx.roundRect(40, 440, ctx.measureText(name).width + 15, 40, 10);
 
   ctx.fillText(name, 48, 468);
 }
