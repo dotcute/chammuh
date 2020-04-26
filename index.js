354c7767aa7f600ae4cee9e2c0098d8c84c0c862
thoratica <admin@thoratica.net>
2020-04-26 20:15:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ ctx.font = '22px Spoqa Han Sans'
 confirm('이름이 뭐야?', 'nameInput', '홍길동', (id, reconfirm) => {
   nickname = document.getElementById(id).value
   if (nickname == '') {
-    alert('이름을 입력해줘!')
-    reconfirm()
+    alert('이름을 입력해줘!').then(
+      reconfirm()
+    )
   } else if (nickname === '참머') {
-    alert('\'참머\' 는 사용할 수 없는 이름이야!')
-    reconfirm()
+    alert('\'참머\' 는 사용할 수 없는 이름이야!').then(
+      reconfirm()
+    )
   } else if (ctx.measureText(nickname).width > 150) {
-    alert('조금 더 짧은 닉네임을 입력해줘! (한글 기준 최대 7자, 알파벳 기준 최대 12자)')
-    reconfirm()
+    alert('조금 더 짧은 닉네임을 입력해줘! (한글 기준 최대 7자, 알파벳 기준 최대 12자)').then(
+      reconfirm()
+    )
   }
 })
 
