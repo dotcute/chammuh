9ba32fae883211033f2b83c5f9faa0615cfcc391
Pneuma <syca0714@gmail.com>
2020-04-26 19:08:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ const confirm = (title, id, placeholder, func) => {
     focusConfirm: false,
     confirmButtonText: '확인'
   }).then((result) => {
-    if (result.value) {
-      func(id);
-    } else {
-      window.location.reload();
-    }
+    func(id, () => confirm(title, id, placeholder, func));
   });
 }
 
@@ -200,17 +196,17 @@ canvas.addEventListener('click', () => {
 let nickname = ''
 ctx.font = '22px Spoqa Han Sans'
 
-confirm('이름이 뭐야?', 'nameInput', '홍길동', (id) => {
+confirm('이름이 뭐야?', 'nameInput', '홍길동', (id, reconfirm) => {
   nickname = document.getElementById(id).value;
   if (nickname == '') {
     alert('이름을 입력해줘!');
-    window.location.reload();
+    reconfirm();
   } else if (nickname === '참머') {
     alert('\'참머\' 는 사용할 수 없는 이름이야!');
-    window.location.reload();
+    reconfirm();
   } else if (ctx.measureText(nickname).width > 150) {
     alert('조금 더 짧은 닉네임을 입력해줘! (한글 기준 최대 7자, 알파벳 기준 최대 12자)');
-    window.location.reload();
+    reconfirm();
   }
 });
 
