87c9434633021042cbf558a9854467964ab6811f
thoratica <admin@thoratica.net>
2020-04-26 20:04:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ let nickname = ''
 ctx.font = '22px Spoqa Han Sans'
 
 confirm('이름이 뭐야?', 'nameInput', '홍길동', (id, reconfirm) => {
-  nickname = document.getElementById(id).value;
+  nickname = document.getElementById(id).value
   if (nickname == '') {
-    alert('이름을 입력해줘!');
-    reconfirm();
+    alert('이름을 입력해줘!')
+    reconfirm()
   } else if (nickname === '참머') {
-    alert('\'참머\' 는 사용할 수 없는 이름이야!');
-    reconfirm();
+    alert('\'참머\' 는 사용할 수 없는 이름이야!')
+    reconfirm()
   } else if (ctx.measureText(nickname).width > 150) {
-    alert('조금 더 짧은 닉네임을 입력해줘! (한글 기준 최대 7자, 알파벳 기준 최대 12자)');
-    reconfirm();
+    alert('조금 더 짧은 닉네임을 입력해줘! (한글 기준 최대 7자, 알파벳 기준 최대 12자)')
+    reconfirm()
   }
-});
+})
 
 fetch('./assets/list.txt')
   .then(async res => preloading('./assets/', (await res.text()).split(', ')))
   .then(() => show('클릭하여 시작...', 'title', false))
   
-fetch('./scripts/script.json').then(async res => playScript(await res.json()));
+fetch('./scripts/script.json').then(async res => playScript(await res.json()))
